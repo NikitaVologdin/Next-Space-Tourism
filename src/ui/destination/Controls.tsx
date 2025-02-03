4b41cbf2ac3871239c3b775c878714213f309863
Nikita Vologdins <nvologdins@gmail.com>
2025-02-03 17:53:47 +0200

    created destination controls transition

--- src/ui/destination/Controls.tsx
+++ src/ui/destination/Controls.tsx
@@ -1,27 +1,43 @@
 "use client";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { motion } from "motion/react";
 
 type props = { refs: string[] };
 
 export default function Controls({ refs }: props) {
   const { slug } = useParams<{ slug: string }>();
+  const hover = { borderColor: "hsl(0,0%,100%,50%)" };
+
+  const active: React.CSSProperties = {
+    position: "absolute",
+    bottom: -3,
+    left: 0,
+    right: 0,
+    height: 3,
+    background: "var(--white)",
+  };
 
   return (
-    <div className="controls destination-controls">
-      {refs.map((ref, index) => {
+    <ul className="controls destination-controls">
+      {refs.map((dest) => {
         return (
-          <Link
-            href={`${ref}`}
-            key={ref + index}
-            className={`controls__button destination-controls__button ${
-              slug === ref ? "destination-controls__button_active" : ""
-            }`}
+          <motion.li
+            className="controls__button destination-controls__button"
+            key={dest}
+            whileHover={hover}
           >
-            {ref}
-          </Link>
+            <Link href={`${dest}`}>{dest}</Link>
+            {slug === dest && (
+              <motion.div
+                style={active}
+                layoutId="dest-controls-underline"
+                id="dest-controls-underline"
+              ></motion.div>
+            )}
+          </motion.li>
         );
       })}
-    </div>
+    </ul>
   );
 }
