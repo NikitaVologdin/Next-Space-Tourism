3092a140bc281f4f52722c3897496ceef3bfbba2
Nikita Vologdins <nvologdins@gmail.com>
2025-02-04 09:24:16 +0200

    improved animation

--- src/ui/Explore.tsx
+++ src/ui/Explore.tsx
@@ -1,10 +1,40 @@
 "use client";
 import Link from "next/link";
+import { motion } from "motion/react";
 
 export default function Explore() {
+  const pulseVariants = {
+    initial: { boxShadow: "0 0 0 0px #ffffffa0" },
+    animate: (delay: number) => {
+      return {
+        boxShadow: "0 0 0 90px #ffffff23",
+        transition: { repeat: Infinity, duration: 3, delay: delay * 1 },
+      };
+    },
+  };
+
   return (
-    <Link href="/destination/mars" className="explore pulse" type="button">
-      Explore
-    </Link>
+    <motion.div
+      className="explore__wrapper"
+      variants={pulseVariants}
+      initial="initial"
+      animate="animate"
+    >
+      <motion.span
+        variants={pulseVariants}
+        custom={1}
+        animate="animate"
+        className="explore__wave"
+      ></motion.span>
+      <motion.span
+        variants={pulseVariants}
+        custom={2}
+        animate="animate"
+        className="explore__wave"
+      ></motion.span>
+      <Link href="/destination/mars" className="explore__link" type="button">
+        Explore
+      </Link>
+    </motion.div>
   );
 }
